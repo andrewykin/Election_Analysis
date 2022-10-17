8b985341ea83767d0cd8853e6c04c03e125ff293
Andrew <andrewykin@gmail.com>
2022-10-17 03:26:37 -0700

    PyPoll Project creation

---
+++ PyPoll.py
@@ -0,0 +1,19 @@
+# Pseudocode for Election Analysis Project
+
+# Assign a variable for the file to load and the path.
+file_to_load = 'Resources\election_results.csv'
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+     # To do: perform analysis.
+     print(election_data)
+
+
+
+# Total number of votes cast
+# A complete list of candidates who received votes
+# Total number of votes each candidate received
+# Percentage of votes each candidate won
+# The winner of the election based on popular vote
+
