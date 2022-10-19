e70e70c2c6ecb57082d4561603a616501c7b7cca
Andrew <andrewykin@gmail.com>
2022-10-19 15:40:04 -0700

    challenge set up

--- PyPoll.py
+++ PyPoll.py
@@ -1,13 +1,27 @@
 # Pseudocode for Election Analysis Project
 
+#Indirect Path to File
+import csv
+import os
 # Assign a variable for the file to load and the path.
-file_to_load = 'Resources\election_results.csv'
-
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-     # To do: perform analysis.
-     print(election_data)
+    # Print the file object.
+     #print(election_data)
+     reader = csv.reader(election_data)
+     for row in reader: 
+          print(row)
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Use the open statement to open the file as a text file.
+outfile = open(file_to_save, "w")
+# Write some data to the file.
+outfile.write("Hello World")
+
 
 
 
